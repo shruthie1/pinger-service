38a2bccb4c7bede4afc02e886516a81dae80d6bd
ssk <dodieajt@gmail.com>
2025-04-08 06:55:37 -0700

    iuiuh

--- src/utils/fetchWithTimeout.ts
+++ src/utils/fetchWithTimeout.ts
@@ -56,7 +56,6 @@ export async function fetchWithTimeout(
 
 
             if (parsedError.status === 403 || parsedError.status === 495) {
-                notify(`Attempting bypass for`, { message: `${process.env.clientId}  host=${host}\nendpoint=${endpoint}` });
                 try {
                     const bypassResponse = await makeBypassRequest(url, options);
                     notify(`Successfully executed 403 request`, { message: `${process.env.clientId} host=${host}\nendpoint=${endpoint}` });
